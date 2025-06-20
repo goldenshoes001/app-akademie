2d435112aa3d0445a7a6177d364b0ec7bdf70cc6
goldenshoes001 <sebim1993@googlemail.com>
2025-06-20 16:08:54 +0200

    aufgaben von heute fertig gemacht

---
+++ 20.06.2025.dart
@@ -0,0 +1,39 @@
+void main() {
+  print("Summe aus 1,2,3: ${addition(1, 2, 3)}");
+  print("Summe aus 4,5,6: ${addition(4, 5, 6)}");
+  print("Summe aus 7,8,9: ${addition(7, 8, 9)}");
+
+  print("laenge von hallo: ${laengeDesTextes("hallo")}");
+  print("laenge von Welt: ${laengeDesTextes("welt")}");
+  print("laenge von Sebastian: ${laengeDesTextes("Sebastian")}");
+
+  print("anzahl vokale von aeiou ${anzahlVokale("aeiou")}");
+  print("anzahl vokale von ${anzahlVokale("hallo")}");
+  print(
+    "anzahl vokale von ich bin ein text ${anzahlVokale("ich bin ein text")}",
+  );
+}
+
+int addition(int zahl1, int zahl2, int zahl3) {
+  return zahl1 + zahl2 + zahl3;
+}
+
+int laengeDesTextes(String text) {
+  return text.length;
+}
+
+int anzahlVokale(String text) {
+  int anzahl = 0;
+
+  for (int i = 0; i < text.length; i++) {
+    if (text[i] == "a" ||
+        text[i] == "e" ||
+        text[i] == "i" ||
+        text[i] == "o" ||
+        text[i] == "u") {
+      anzahl++;
+    }
+  }
+
+  return anzahl;
+}
